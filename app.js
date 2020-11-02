635d7afc92f15fc42a18e2431abdbc07d3b953a9
GerryMengis <geremengis@gmail.com>
2020-11-01 18:38:11 -0800

    post & delete request updated

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // routes
@@ -35,6 +36,10 @@ app.get('/about', (req, res) => {
 });
 
 // blog routes
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create new Blog' });
+});
+
 app.get('/blogs', (req, res) => {
     Blog.find().sort({ createdAt: -1}) //page display decending order
         .then((result) => {
@@ -44,11 +49,42 @@ app.get('/blogs', (req, res) => {
             console.log(err); 
         });
 })
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', {title: 'Create new Blog' });
+app.post('/blogs', (req, res) => {
+    // console.log(req.body);
+    const blog = new Blog(req.body);
+  
+    blog.save()
+      .then(result => {
+        res.redirect('/blogs');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
+    
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    
+    Blog.findByIdAndDelete(id)
+    .then(result => {
+        res.json({ redirect: '/blogs' });
+    })
+    .catch(err => {
+        console.log(err);
+    });
+})
 
 // 404 page
 app.use((req, res) => {
